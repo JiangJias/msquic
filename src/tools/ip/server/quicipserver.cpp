JiangJias/msquic

commit 6000e137d9756343b3851105ee0a4343b72857da

Public IP Lookup Protocol/Tool (#601)

src/tools/ip/server/quicipserver.cpp
@@ -1,0 +1,277 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    A server implementation for a public IP lookup protocol.
+
+TODO:
+
+    Don't use the QUIC_ADDR format, as it's not cross-platform. Use the same
+    format as the QUIC spec's preferred address TP.
+
+--*/
+
+#define QUIC_TEST_APIS 1 // Needed for self signed cert API
+#include <msquichelper.h>
+
+const QUIC_REGISTRATION_CONFIG RegConfig = { "ip", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+const QUIC_BUFFER Alpn = { sizeof("ip") - 1, (uint8_t*)"ip" };
+const uint16_t UdpPort = 4444;
+const uint64_t IdleTimeoutMs = 2000;
+
+const QUIC_API_TABLE* MsQuic;
+HQUIC Registration;
+HQUIC Session;
+QUIC_SEC_CONFIG* SecurityConfig;
+
+extern "C" void QuicTraceRundown(void) { }
+
+void
+PrintUsage()
+{
+    printf("\nquicip runs a public IP lookup server.\n\n");
+
+    printf("Usage:\n");
+    printf("  quicipserver.exe -selfsign or -cert_hash:<...> [and -cert_store:<...> | -machine] or (-cert_file:<...> and -key_file:<...>)\n");
+}
+
+QUIC_STATUS
+QUIC_API
+ServerStreamCallback(
+    _In_ HQUIC Stream,
+    _In_opt_ void* /* Context */,
+    _Inout_ QUIC_STREAM_EVENT* Event
+    )
+{
+    switch (Event->Type) {
+    case QUIC_STREAM_EVENT_SEND_COMPLETE:
+        QUIC_FREE(Event->SEND_COMPLETE.ClientContext);
+        break;
+    case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
+        MsQuic->StreamClose(Stream);
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+void
+ServerSendIp(
+    _In_ HQUIC Connection
+    )
+{
+    QUIC_STATUS Status;
+    HQUIC Stream = nullptr;
+
+    auto SendBufferRaw = QUIC_ALLOC_PAGED(sizeof(QUIC_BUFFER) + sizeof(QUIC_ADDR));
+    if (SendBufferRaw == nullptr) {
+        return;
+    }
+
+    auto SendBuffer = (QUIC_BUFFER*)SendBufferRaw;
+    SendBuffer->Buffer = (uint8_t*)SendBufferRaw + sizeof(QUIC_BUFFER);
+    SendBuffer->Length = sizeof(QUIC_ADDR);
+
+    if (QUIC_FAILED(Status = MsQuic->GetParam(Connection, QUIC_PARAM_LEVEL_CONNECTION, QUIC_PARAM_CONN_REMOTE_ADDRESS, &SendBuffer->Length, SendBuffer->Buffer))) {
+        printf("GetParam(CONN_REMOTE_ADDRESS) failed, 0x%x!\n", Status);
+        QUIC_FREE(SendBuffer);
+        return;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->StreamOpen(Connection, QUIC_STREAM_OPEN_FLAG_UNIDIRECTIONAL, ServerStreamCallback, nullptr, &Stream))) {
+        printf("StreamOpen failed, 0x%x!\n", Status);
+        return;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->StreamStart(Stream, QUIC_STREAM_START_FLAG_NONE))) {
+        printf("StreamStart failed, 0x%x!\n", Status);
+        MsQuic->StreamClose(Stream);
+        return;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->StreamSend(Stream, SendBuffer, 1, QUIC_SEND_FLAG_FIN, SendBuffer))) {
+        printf("StreamSend failed, 0x%x!\n", Status);
+        QUIC_FREE(SendBufferRaw);
+        MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
+    }
+}
+
+QUIC_STATUS
+QUIC_API
+ServerConnectionCallback(
+    _In_ HQUIC Connection,
+    _In_opt_ void* /* Context */,
+    _Inout_ QUIC_CONNECTION_EVENT* Event
+    )
+{
+    switch (Event->Type) {
+    case QUIC_CONNECTION_EVENT_CONNECTED:
+        MsQuic->ConnectionSendResumptionTicket(Connection, QUIC_SEND_RESUMPTION_FLAG_NONE, 0, NULL);
+        ServerSendIp(Connection);
+        break;
+    case QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE:
+        MsQuic->ConnectionClose(Connection);
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+QUIC_STATUS
+QUIC_API
+ServerListenerCallback(
+    _In_ HQUIC /* Listener */,
+    _In_opt_ void* /* Context */,
+    _Inout_ QUIC_LISTENER_EVENT* Event
+    )
+{
+    switch (Event->Type) {
+    case QUIC_LISTENER_EVENT_NEW_CONNECTION:
+        Event->NEW_CONNECTION.SecurityConfig = SecurityConfig;
+        MsQuic->SetCallbackHandler(Event->NEW_CONNECTION.Connection, (void*)ServerConnectionCallback, nullptr);
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+void
+RunServer(
+    _In_ int argc,
+    _In_reads_(argc) _Null_terminated_ char* argv[]
+    )
+{
+    QUIC_STATUS Status;
+    HQUIC Listener = nullptr;
+
+    QUIC_ADDR Address = {};
+    QuicAddrSetFamily(&Address, AF_UNSPEC);
+    QuicAddrSetPort(&Address, UdpPort);
+
+    QUIC_SEC_CONFIG_PARAMS* SelfSignedCertParams = nullptr;
+    const char* Cert;
+    const char* KeyFile;
+    if (GetValue(argc, argv, "selfsign")) {
+        SelfSignedCertParams = QuicPlatGetSelfSignedCert(QUIC_SELF_SIGN_CERT_USER);
+        if (!SelfSignedCertParams) {
+            printf("Failed to create platform self signed certificate\n");
+            return;
+        }
+
+        SecurityConfig = GetSecConfigForSelfSigned(MsQuic, Registration, SelfSignedCertParams);
+        if (!SecurityConfig) {
+            printf("Failed to create security config for self signed certificate\n");
+            QuicPlatFreeSelfSignedCert(SelfSignedCertParams);
+            return;
+        }
+    } else if (TryGetValue(argc, argv, "cert_hash", &Cert)) {
+        const char* StoreName = "My";
+        QUIC_CERTIFICATE_HASH_STORE_FLAGS Flags = QUIC_CERTIFICATE_HASH_STORE_FLAG_NONE;
+        if (GetValue(argc, argv, "machine")) {
+            Flags |= QUIC_CERTIFICATE_HASH_STORE_FLAG_MACHINE_STORE;
+        }
+        TryGetValue(argc, argv, "cert_store", &StoreName);
+        SecurityConfig = GetSecConfigForThumbprintAndStore(MsQuic, Registration, Flags, Cert, StoreName);
+        if (SecurityConfig == nullptr) {
+            printf("Failed to load certificate from hash!\n");
+            return;
+        }
+    } else if (TryGetValue(argc, argv, "cert_file", &Cert) &&
+        TryGetValue(argc, argv, "key_file", &KeyFile)) {
+        SecurityConfig = GetSecConfigForFile(MsQuic, Registration, KeyFile, Cert);
+        if (SecurityConfig == nullptr) {
+            printf("Failed to load certificate from file!\n");
+            return;
+        }
+    } else {
+        printf("Must specify 'selfsign', '-cert_hash' or 'cert_file'!\n");
+        return;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->ListenerOpen(Session, ServerListenerCallback, nullptr, &Listener))) {
+        printf("ListenerOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->ListenerStart(Listener, &Address))) {
+        printf("ListenerStart failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    printf("Press Enter to exit.\n\n");
+    getchar();
+
+Error:
+
+    if (Listener != nullptr) {
+        MsQuic->ListenerClose(Listener);
+    }
+    MsQuic->SecConfigDelete(SecurityConfig);
+    if (SelfSignedCertParams) {
+        QuicPlatFreeSelfSignedCert(SelfSignedCertParams);
+    }
+}
+
+int
+QUIC_MAIN_EXPORT
+main(
+    _In_ int argc,
+    _In_reads_(argc) _Null_terminated_ char* argv[]
+    )
+{
+    QuicPlatformSystemLoad();
+
+    QUIC_STATUS Status = QUIC_STATUS_SUCCESS;
+    if (QUIC_FAILED(Status = QuicPlatformInitialize())) {
+        printf("QuicPlatformInitialize failed, 0x%x!\n", Status);
+        QuicPlatformSystemUnload();
+        return Status;
+    }
+
+    if (QUIC_FAILED(Status = MsQuicOpen(&MsQuic))) {
+        printf("MsQuicOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->RegistrationOpen(&RegConfig, &Registration))) {
+        printf("RegistrationOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->SessionOpen(Registration, &Alpn, 1, nullptr, &Session))) {
+        printf("SessionOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = MsQuic->SetParam(
+            Session, QUIC_PARAM_LEVEL_SESSION, QUIC_PARAM_SESSION_IDLE_TIMEOUT,
+            sizeof(IdleTimeoutMs), &IdleTimeoutMs))) {
+        printf("SetParam(QUIC_PARAM_SESSION_IDLE_TIMEOUT) failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    RunServer(argc, argv);
+
+Error:
+
+    if (MsQuic != nullptr) {
+        if (Session != nullptr) {
+            MsQuic->SessionClose(Session); // Waits on all connections to be cleaned up.
+        }
+        if (Registration != nullptr) {
+            MsQuic->RegistrationClose(Registration);
+        }
+        MsQuicClose(MsQuic);
+    }
+
+    QuicPlatformUninitialize();
+    QuicPlatformSystemUnload();
+
+    return (int)Status;
+}

CMakeLists.txt
@@ -319,6 +319,8 @@ if(QUIC_BUILD_TOOLS)
     add_subdirectory(src/tools/attack)
     add_subdirectory(src/tools/interop)
     add_subdirectory(src/tools/interopserver)
+    add_subdirectory(src/tools/ip/client)
+    add_subdirectory(src/tools/ip/server)
     add_subdirectory(src/tools/ping)
     add_subdirectory(src/tools/post)
     add_subdirectory(src/tools/reach)

src/inc/msquic_linux.h
@@ -185,11 +185,18 @@ extern "C" {
 #define QUIC_CERTIFICATE_FLAG_IGNORE_WEAK_SIGNATURE             0x00010000
 
 //
+// Helpers for Windows string functions.
+//
+
+#define _strnicmp strncasecmp
+#define sprintf_s(dst, dst_len, format, ...) sprintf(dst, format, __VA_ARGS__)
+#define _vsnprintf_s(dst, dst_len, flag, format, ...) vsnprintf(dst, dst_len, format, __VA_ARGS__)
+
+//
 // Hack to pass in a client trusted cert from APP layer to the TAL layer.
 //
 
 extern char *QuicOpenSslClientTrustedCert;
-
 
 //
 // IP Address Abstraction Helpers

src/inc/quic_platform_linux.h
@@ -177,18 +177,6 @@ InterlockedIncrement64(
 }
 
 //
-// String utils.
-//
-
-int _strnicmp(const char * _Str1, const char * _Str2, size_t _MaxCount);
-
-#define sprintf_s(dst, dst_len, format, ...) \
-    sprintf(dst, format, __VA_ARGS__)
-
-#define _vsnprintf_s(dst, dst_len, flag, format, ...) \
-    vsnprintf(dst, dst_len, format, __VA_ARGS__)
-
-//
 // Assertion interfaces.
 //
 

src/platform/platform_linux.c
@@ -612,16 +612,6 @@ QuicConvertFromMappedV6(
     }
 }
 
-int
-_strnicmp(
-    _In_ const char * _Str1,
-    _In_ const char * _Str2,
-    _In_ size_t _MaxCount
-    )
-{
-    return strncasecmp(_Str1, _Str2, _MaxCount);
-}
-
 QUIC_STATUS
 QuicThreadCreate(
     _In_ QUIC_THREAD_CONFIG* Config,

src/tools/ip/client/CMakeLists.txt
@@ -1,0 +1,17 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${QUIC_CXX_FLAGS}")
+
+include_directories(${PROJECT_SOURCE_DIR}/src/tools/ip)
+
+add_executable(quicipclient quicipclient.cpp)
+target_compile_options(quicipclient PRIVATE
+     $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+        ${CLANG_GCC_WARNING_FLAGS}>
+     $<$<CXX_COMPILER_ID:MSVC>:
+        ${MSVC_WARNING_FLAGS}>)
+
+target_link_libraries(quicipclient msquic)
+
+set_property(TARGET quicipclient PROPERTY FOLDER "tools")

src/tools/ip/client/quicipclient.cpp
@@ -1,0 +1,56 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    A command-line wrapper for MsQuicGetPublicIP.
+
+--*/
+
+#include "msquichelper.h"
+#include "quicip.h"
+
+int
+QUIC_MAIN_EXPORT
+main(
+    _In_ int argc,
+    _In_reads_(argc) _Null_terminated_ char* argv[]
+    )
+{
+    if (GetValue(argc, argv, "?")) {
+        printf("Usage:\n");
+        printf("  quicipclient.exe [-target:<...>] [-local:<...>] [-unsecure]\n");
+        return 0;
+    }
+
+    const char* Target = "quic.westus.cloudapp.azure.com";
+    const char* LocalAddressArg = "*";
+    bool Unsecure = false;
+    QUIC_ADDR LocalAddress;
+    QUIC_ADDR PublicAddress;
+
+    TryGetValue(argc, argv, "target", &Target);
+    TryGetValue(argc, argv, "local", &LocalAddressArg);
+    if (GetValue(argc, argv, "unsecure")) {
+        Unsecure = true;
+    }
+
+    if (!ConvertArgToAddress(LocalAddressArg, 0, &LocalAddress)) {
+        printf("Failed to decode IP address.\n");
+        return 0;
+    }
+
+    if (MsQuicGetPublicIP(Target, Unsecure, &LocalAddress, &PublicAddress)) {
+        QUIC_ADDR_STR AddrStr = { 0 };
+        QuicAddrToString(&LocalAddress, &AddrStr);
+        printf(" Local IP: %s\n", AddrStr.Address);
+        QuicAddrToString(&PublicAddress, &AddrStr);
+        printf("Public IP: %s\n", AddrStr.Address);
+    } else {
+        printf("Failed.\n");
+    }
+
+    return 0;
+}

src/tools/ip/quicip.h
@@ -1,0 +1,177 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    A client helper for resolving the public IP address.
+
+TODO:
+
+    Don't use the QUIC_ADDR format, as it's not cross-platform. Use the same
+    format as the QUIC spec's preferred address TP.
+
+--*/
+
+#pragma once
+
+#include <msquic.h>
+#include <stdio.h>
+
+typedef struct CALLBACK_CONTEXT {
+    const QUIC_API_TABLE* MsQuic;
+    BOOLEAN Success;
+    QUIC_ADDR* LocalAdrress;
+    QUIC_ADDR* PublicAddress;
+} CALLBACK_CONTEXT;
+
+QUIC_STATUS
+QUIC_API
+ClientStreamCallback(
+    _In_ HQUIC Stream,
+    _In_opt_ void* _Context,
+    _Inout_ QUIC_STREAM_EVENT* Event
+    )
+{
+    CALLBACK_CONTEXT* Context = (CALLBACK_CONTEXT*)_Context;
+    switch (Event->Type) {
+    case QUIC_STREAM_EVENT_RECEIVE:
+        if (Event->RECEIVE.AbsoluteOffset + Event->RECEIVE.TotalBufferLength <= sizeof(QUIC_ADDR)) {
+            uint64_t Offset = Event->RECEIVE.AbsoluteOffset;
+            for (uint32_t i = 0; i < Event->RECEIVE.BufferCount; ++i) {
+                memcpy(
+                    ((uint8_t*)Context->PublicAddress) + Offset,
+                    Event->RECEIVE.Buffers[i].Buffer,
+                    Event->RECEIVE.Buffers[i].Length);
+                Offset += Event->RECEIVE.Buffers[i].Length;
+            }
+        }
+        break;
+    case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN: {
+        Context->Success = TRUE;
+        Context->MsQuic->ConnectionShutdown(Stream, QUIC_CONNECTION_SHUTDOWN_FLAG_NONE, 0);
+        uint32_t LocalAddressLength = sizeof(QUIC_ADDR);
+        Context->MsQuic->GetParam(Stream, QUIC_PARAM_LEVEL_CONNECTION, QUIC_PARAM_CONN_LOCAL_ADDRESS, &LocalAddressLength, Context->LocalAdrress);
+        break;
+    }
+    case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
+        Context->MsQuic->StreamClose(Stream);
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+QUIC_STATUS
+QUIC_API
+ClientConnectionCallback(
+    _In_ HQUIC Connection,
+    _In_opt_ void* _Context,
+    _Inout_ QUIC_CONNECTION_EVENT* Event
+    )
+{
+    CALLBACK_CONTEXT* Context = (CALLBACK_CONTEXT*)_Context;
+    switch (Event->Type) {
+    case QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE:
+        Context->MsQuic->ConnectionClose(Connection);
+        break;
+    case QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED:
+        Context->MsQuic->SetCallbackHandler(Event->PEER_STREAM_STARTED.Stream, (void*)ClientStreamCallback, Context);
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+BOOLEAN
+MsQuicGetPublicIP(
+    _In_ const char* Target,
+    _In_ BOOLEAN Unsecure,
+    _In_ QUIC_ADDR* LocalAddress, // Can be unspecified
+    _Out_ QUIC_ADDR* PublicAddress
+    )
+{
+    QUIC_STATUS Status = QUIC_STATUS_SUCCESS;
+
+    const QUIC_REGISTRATION_CONFIG RegConfig = { "ip", QUIC_EXECUTION_PROFILE_TYPE_SCAVENGER };
+    const QUIC_BUFFER Alpn = { sizeof("ip") - 1, (uint8_t*)"ip" };
+    const uint16_t UdpPort = 4444;
+    const uint64_t IdleTimeoutMs = 2000;
+    const uint16_t PeerStreamCount = 1;
+
+    HQUIC Registration = nullptr;
+    HQUIC Session = nullptr;
+    HQUIC Connection = nullptr;
+
+    CALLBACK_CONTEXT Context = { NULL, FALSE, LocalAddress, PublicAddress };
+
+    if (QUIC_FAILED(Status = MsQuicOpen(&Context.MsQuic))) {
+        printf("MsQuicOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->RegistrationOpen(&RegConfig, &Registration))) {
+        printf("RegistrationOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->SessionOpen(Registration, &Alpn, 1, nullptr, &Session))) {
+        printf("SessionOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->SetParam(Session, QUIC_PARAM_LEVEL_SESSION, QUIC_PARAM_SESSION_IDLE_TIMEOUT, sizeof(IdleTimeoutMs), &IdleTimeoutMs))) {
+        printf("SetParam(SESSION_IDLE_TIMEOUT) failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->SetParam(Session, QUIC_PARAM_LEVEL_SESSION, QUIC_PARAM_SESSION_PEER_UNIDI_STREAM_COUNT, sizeof(PeerStreamCount), &PeerStreamCount))) {
+        printf("SetParam(SESSION_PEER_UNIDI_STREAM_COUNT) failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->ConnectionOpen(Session, ClientConnectionCallback, &Context, &Connection))) {
+        printf("ConnectionOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    if (Unsecure) {
+        const uint32_t CertificateValidationFlags = QUIC_CERTIFICATE_FLAG_DISABLE_CERT_VALIDATION;
+        if (QUIC_FAILED(Status = Context.MsQuic->SetParam(Connection, QUIC_PARAM_LEVEL_CONNECTION, QUIC_PARAM_CONN_CERT_VALIDATION_FLAGS, sizeof(CertificateValidationFlags), &CertificateValidationFlags))) {
+            printf("SetParam(CONN_CERT_VALIDATION_FLAGS) failed, 0x%x!\n", Status);
+            goto Error;
+        }
+    }
+
+    if (AF_UNSPEC != QuicAddrGetFamily(LocalAddress)) {
+        if (QUIC_FAILED(Status = Context.MsQuic->SetParam(Connection, QUIC_PARAM_LEVEL_CONNECTION, QUIC_PARAM_CONN_LOCAL_ADDRESS, sizeof(QUIC_ADDR), LocalAddress))) {
+            printf("SetParam(CONN_LOCAL_ADDRESS) failed, 0x%x!\n", Status);
+            goto Error;
+        }
+    }
+
+    if (QUIC_FAILED(Status = Context.MsQuic->ConnectionStart(Connection, QuicAddrGetFamily(LocalAddress), Target, UdpPort))) {
+        printf("ConnectionStart failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+Error:
+
+    if (Context.MsQuic) {
+        if (QUIC_FAILED(Status) && Connection) {
+            Context.MsQuic->ConnectionClose(Connection);
+        }
+        if (Session) {
+            Context.MsQuic->SessionClose(Session); // Waits on all connections to be cleaned up.
+        }
+        if (Registration) {
+            Context.MsQuic->RegistrationClose(Registration);
+        }
+        MsQuicClose(Context.MsQuic);
+    }
+
+    return Context.Success;
+}

commit c5f84bfa35ff2895b7218ec289aeb4b3f24f1ec0

Add CXPLAT_FREE() to avoid potential memory leak
Add CXPLAT_FREE() in all the error paths to avoid potential memory leak

Fixes: 6000e137 ("Public IP Lookup Protocol/Tool (#601)")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

src/tools/ip/server/quicipserver.cpp
@@ -81,11 +81,13 @@ ServerSendIp(
 
     if (QUIC_FAILED(Status = MsQuic->StreamOpen(Connection, QUIC_STREAM_OPEN_FLAG_UNIDIRECTIONAL, ServerStreamCallback, nullptr, &Stream))) {
         printf("StreamOpen failed, 0x%x!\n", Status);
+        CXPLAT_FREE(SendBuffer, QUIC_POOL_TOOL);
         return;
     }
 
     if (QUIC_FAILED(Status = MsQuic->StreamStart(Stream, QUIC_STREAM_START_FLAG_NONE))) {
         printf("StreamStart failed, 0x%x!\n", Status);
+        CXPLAT_FREE(SendBuffer, QUIC_POOL_TOOL);
         MsQuic->StreamClose(Stream);
         return;
     }