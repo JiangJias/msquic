cd5a0f49c9b6fc0a7d7364090874fee802de8d38
Jiasheng Jiang <jiashengjiangcool@gmail.com>
2025-07-04 02:31:55 +0000

    Add check for memory allocation APIs
    Add check for the return value of CXPLAT_ALLOC_NONPAGED() and CxPlatPoolAlloc() to avoid potential NULL pointer dereference.
    
    Fixes: 7da6fea9 ("Add app-owned scatter/gather buffer management (#4758)")
    Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

--- src/core/unittest/RecvBufferTest.cpp
+++ src/core/unittest/RecvBufferTest.cpp
@@ -55,6 +55,9 @@ struct RecvBuffer {
                 PreallocChunk = (QUIC_RECV_CHUNK*)CXPLAT_ALLOC_NONPAGED(
                     sizeof(QUIC_RECV_CHUNK) + AllocBufferLength,
                     QUIC_POOL_RECVBUF); // Use the recv buffer pool tag as this memory is moved to the recv buffer.
+            if (PreallocChunk == nullptr) {
+                return QUIC_STATUS_OUT_OF_MEMORY;
+            }
             QuicRecvChunkInitialize(PreallocChunk, AllocBufferLength, (uint8_t*)(PreallocChunk + 1), FALSE);
         }
         printf("Initializing: [mode=%u,vlen=%u,alen=%u]\n", RecvMode, VirtualBufferLength, AllocBufferLength);
@@ -75,11 +78,20 @@ struct RecvBuffer {
             CXPLAT_LIST_ENTRY ChunkList;
             CxPlatListInitializeHead(&ChunkList);
             AppOwnedBuffer = (uint8_t *)CXPLAT_ALLOC_NONPAGED(VirtualBufferLength, QUIC_POOL_TEST);
+            if (AppOwnedBuffer == nullptr) {
+                return QUIC_STATUS_OUT_OF_MEMORY;
+            }
             auto* Chunk = (QUIC_RECV_CHUNK *)CxPlatPoolAlloc(&AppBufferChunkPool);
+            if (Chunk == nullptr) {
+                return QUIC_STATUS_OUT_OF_MEMORY;
+            }
             QuicRecvChunkInitialize(Chunk, AllocBufferLength, AppOwnedBuffer, TRUE);
             CxPlatListInsertHead(&ChunkList, &Chunk->Link);
             if (VirtualBufferLength > AllocBufferLength) {
                 auto* Chunk2 = (QUIC_RECV_CHUNK *)CxPlatPoolAlloc(&AppBufferChunkPool);
+                if (Chunk2 == nullptr) {
+                    return QUIC_STATUS_OUT_OF_MEMORY;
+                }
                 QuicRecvChunkInitialize(Chunk2, VirtualBufferLength - AllocBufferLength, AppOwnedBuffer + AllocBufferLength, TRUE);
                 CxPlatListInsertTail(&ChunkList, &Chunk2->Link);
             }
@@ -281,6 +293,9 @@ struct RecvBuffer {
         CxPlatListInitializeHead(ChunkList);
         for (auto size: ChunkSizes) {
             auto* chunk = reinterpret_cast<QUIC_RECV_CHUNK *>(CxPlatPoolAlloc(&AppBufferChunkPool));
+            if (chunk == nullptr) {
+                return QUIC_STATUS_OUT_OF_MEMORY;
+            }
             QuicRecvChunkInitialize(chunk, size, Buffer, TRUE);
             Buffer = Buffer + size;
             CxPlatListInsertTail(ChunkList, &chunk->Link);
